24f8303ea4bb61a67bf749c0cd7fdede680713f9
jackalwei <jackal8125@gmail.com>
2016-06-04 16:19:42 +0800

    finish Index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,34 @@
 class BooksController < ApplicationController
 
+  #首頁
+  #Get books_path
   def index
+    @books=Book.page(params[:page]).per(8)
   end
+
+  #CRUD的ACTION
+  #Post books_path
+  def create
+  end
+
+  #Get book_path(:id)
+  def show
+  end
+
+  #Patch book_path(:id)
+  def update
+  end
+
+  #DELETE book_path(:id)
+  def destroy
+  end
+
+  #new_book_path
+  def new
+  end
+
+  #edit_book_path
+  def edit
+  end
+
 end
